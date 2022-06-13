9f43629234c5d5a91d556d0f461ee85b892e4f16
VishMur <vishwamurugappan@gmail.com>
2022-06-12 19:03:38 -0600

    finalizing dat generation

--- Scraping.py
+++ Scraping.py
@@ -23,13 +23,11 @@ import glob
 import requests
 
 all_data = pd.DataFrame()
-for file in glob.glob("rawCSV/*"):
+for file in glob.glob("rawcsv/*"):
     city_data = pd.read_csv(file)
     all_data = all_data.append(city_data)
-
-for id, element in enumerate(all_data['web-scraper-order']):
-    element = element[-3:]
-    all_data['web-scraper-order'][id] = element
+    print("Checkpoint 1: City Added")
+print("Checkpoint 2 (Number of Cities Total):", len(all_data))
 
 def combiner(df):
     odds = list(range(1, len(df) + 1, 2))
@@ -40,10 +38,13 @@ def combiner(df):
         even_number = evens[number]
         combined_datapoint = df.iloc[odd_number-1].combine_first(df.iloc[even_number-1])
         combined_all_data = combined_all_data.append(combined_datapoint)
+    print("Checkpoint 4:", combined_all_data)
     return combined_all_data
 
 
 all_data = all_data.groupby('web-scraper-start-url')[['name', 'rating', 'website', 'location', 'wheelchair', 'family', 'image_link-src', 'description']].first()
+print("Checkpoint 3:", all_data)
+
 combined_all_data = combiner(all_data)
 
 description_list = []
@@ -55,6 +56,7 @@ for url in combined_all_data['website']:
             metas = soup.find_all('meta')
             description = [ meta.attrs['content'] for meta in metas if 'name' in meta.attrs and meta.attrs['name'] == 'description' ]
             description_list.append(''.join(description))
+            print("Checkpoint 5:", description_list)
         except:
             try:
                 page = requests.get('http://' + url)
@@ -62,24 +64,38 @@ for url in combined_all_data['website']:
                 metas = soup.find_all('meta')
                 description = [ meta.attrs['content'] for meta in metas if 'name' in meta.attrs and meta.attrs['name'] == 'description' ]
                 description_list.append(''.join(description))
+                print("Checkpoint 5:", description_list)
             except:
                 description_list.append(None)
+                print("Checkpoint 5:", description_list)
     else:
         description_list.append(None)
+        print("Checkpoint 5:", description_list)
+
+print("Checkpoint 6 (Full Description List Length):", len(description_list))
 
 locator = geocoders.GoogleV3(api_key="AIzaSyDUWchqTkzvxteGNb_tDPCR3jvL1k5J_g4", user_agent="scraping_test.ipynb")
 latitude_list = []
 longitude_list = []
 for location in combined_all_data['location']:
-    coordinates = locator.geocode(location)
-    latitude = coordinates.latitude
-    longitude = coordinates.longitude
-    latitude_list.append(latitude)
-    longitude_list.append(longitude)
+    try:
+        coordinates = locator.geocode(location)
+        latitude = coordinates.latitude
+        longitude = coordinates.longitude
+        latitude_list.append(latitude)
+        longitude_list.append(longitude)
+        print("Checkpoint 7:", latitude_list)
+    except:
+        latitude_list.append(None)
+        longitude_list.append(None)
+        print("Checkpoint 7:", latitude_list)
 
 combined_all_data['meta_descriptions'] = description_list
 combined_all_data['latitude'] = latitude_list
 combined_all_data['longitude'] = longitude_list
 combined_all_data = combined_all_data.reset_index(drop = True)
 
+print("Checkpoint 8 (Final Dataframe created)")
+
 combined_all_data.to_csv('complete_data.csv')
+print("Checkpoint 9 (Process Complete)")
