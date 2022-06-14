0ccd5cceaec7d46403031c540cacee96ff052d5f
VishMur <vishwamurugappan@gmail.com>
2022-06-14 00:01:28 -0600

    changes

--- attraction_picture_downloader.py
+++ attraction_picture_downloader.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import wget
+import os
 
 complete_data = pd.read_csv("more_data.csv")
-links = list(complete_data['image_link-src'].astype("str"))[27:]
-print(links)
-cities = list(complete_data['city'].astype("str"))[27:]
+links = list(complete_data['image_link-src'].astype("str"))[12:]
+cities = list(complete_data['city'].astype("str"))[12:]
 
 
-for idx, city in enumerate(cities, 1):
+for idx, city in enumerate(cities, 13):
     try:
         file_name = str(idx) + " - " + city + ".jpg"
         url = links[idx - 1]
 
         old_file_name = wget.download(url)
-        #os.rename(old_file_name, file_name)
+        os.rename(old_file_name, file_name)
         print(file_name, "downloaded!")
     except:
         file_name = str(idx) + " - " + city + ".txt"
