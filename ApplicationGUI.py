fa61c3192781f45dcccb7d198d753d15a28aedf7
Dheeraj Vislawath <57468596+DeerEdge@users.noreply.github.com>
2022-06-24 02:17:39 -0500

    Q and A window collapsible boxes

--- ApplicationGUI.py
+++ ApplicationGUI.py
@@ -2,6 +2,8 @@ import os
 import webbrowser
 import io
 # folium v0.12.1 - Used to display geographical data
+from random import random
+
 import folium
 import ApplicationDatabase
 import ApplicationFilterRequest
@@ -26,6 +28,79 @@ geolocator = Nominatim(user_agent="geoapiExercises")
 client = IpregistryClient("72bw4jakulj27ism")
 ipInfo = client.lookup()
 
+class CollapsibleBox(QtWidgets.QWidget):
+    def __init__(self, title="", parent=None):
+        super(CollapsibleBox, self).__init__(parent)
+
+        self.toggle_button = QtWidgets.QToolButton(
+            text=title, checkable=True, checked=False
+        )
+        self.toggle_button.setStyleSheet("QToolButton { border: none; }")
+        self.toggle_button.setToolButtonStyle(
+            QtCore.Qt.ToolButtonTextBesideIcon
+        )
+        self.toggle_button.setArrowType(QtCore.Qt.RightArrow)
+        self.toggle_button.pressed.connect(self.on_pressed)
+
+        self.toggle_animation = QtCore.QParallelAnimationGroup(self)
+
+        self.content_area = QtWidgets.QScrollArea(
+            maximumHeight=0, minimumHeight=0
+        )
+        self.content_area.setSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed
+        )
+        self.content_area.setFrameShape(QtWidgets.QFrame.NoFrame)
+
+        lay = QtWidgets.QVBoxLayout(self)
+        lay.setSpacing(0)
+        lay.setContentsMargins(0, 0, 0, 0)
+        lay.addWidget(self.toggle_button)
+        lay.addWidget(self.content_area)
+
+        self.toggle_animation.addAnimation(
+            QtCore.QPropertyAnimation(self, b"minimumHeight")
+        )
+        self.toggle_animation.addAnimation(
+            QtCore.QPropertyAnimation(self, b"maximumHeight")
+        )
+        self.toggle_animation.addAnimation(
+            QtCore.QPropertyAnimation(self.content_area, b"maximumHeight")
+        )
+
+    @QtCore.pyqtSlot()
+    def on_pressed(self):
+        checked = self.toggle_button.isChecked()
+        self.toggle_button.setArrowType(
+            QtCore.Qt.DownArrow if not checked else QtCore.Qt.RightArrow
+        )
+        self.toggle_animation.setDirection(
+            QtCore.QAbstractAnimation.Forward
+            if not checked
+            else QtCore.QAbstractAnimation.Backward
+        )
+        self.toggle_animation.start()
+
+    def setContentLayout(self, layout):
+        lay = self.content_area.layout()
+        del lay
+        self.content_area.setLayout(layout)
+        collapsed_height = (
+            self.sizeHint().height() - self.content_area.maximumHeight()
+        )
+        content_height = layout.sizeHint().height()
+        for i in range(self.toggle_animation.animationCount()):
+            animation = self.toggle_animation.animationAt(i)
+            animation.setDuration(500)
+            animation.setStartValue(collapsed_height)
+            animation.setEndValue(collapsed_height + content_height)
+
+        content_animation = self.toggle_animation.animationAt(
+            self.toggle_animation.animationCount() - 1
+        )
+        content_animation.setDuration(500)
+        content_animation.setStartValue(0)
+        content_animation.setEndValue(content_height)
 
 # Creates an the instance of QApplication, and creates all corresponding class functions
 class Ui_MainWindow(object):
@@ -908,33 +983,44 @@ class Ui_MainWindow(object):
         self.QandA_window.setFixedSize(800, 600)
         self.QandA_window.setWindowTitle("Frequently Asked Questions and their Answers")
 
-        combobox1 = QtWidgets.QComboBox(self.QandA_window)
-        combobox1.addItem('One')
-        combobox1.addItem('Two')
-        combobox1.addItem('Three')
-        combobox1.addItem('Four')
-
         self.QandA_window_central_widget = QtWidgets.QWidget(self.QandA_window)
         self.QandA_window_central_widget.setFixedSize(800, 600)
 
-        self.QandA_window_QGroupBox = QtWidgets.QGroupBox(self.QandA_window_central_widget)
-        self.QandA_window_QGroupBox.setFixedSize(784, 554)
-        self.QandA_window_QGroupBox.move(8, 35)
-        self.QandA_window_QGroupBox.setEnabled(True)
-        self.QandA_window_QGroupBox.setFlat(True)
-        self.QandA_window_QGroupBox.setObjectName("QandATextContainer")
+        scroll = QtWidgets.QScrollArea(self.QandA_window_central_widget)
+        content = QtWidgets.QWidget()
+        scroll.setWidget(content)
+        scroll.setWidgetResizable(True)
+        vlay = QtWidgets.QVBoxLayout(content)
+        for i in range(10):
+            box = CollapsibleBox("Collapsible Box Header-{}".format(i))
+            vlay.addWidget(box)
+            lay = QtWidgets.QVBoxLayout()
+            for j in range(8):
+                label = QtWidgets.QLabel("{}".format(j))
+                label.setAlignment(QtCore.Qt.AlignCenter)
+                lay.addWidget(label)
 
-        self.QandA_QLabel = self.create_QLabel("QandA_window_QGroupBox", 343, 0, 200, 50)
-        self.QandA_QLabel.setText("QandA")
-        self.QandA_QLabel.setObjectName("QandATitle")
+            box.setContentLayout(lay)
+        vlay.addStretch()
 
-        self.QandA = QtWidgets.QPlainTextEdit(self.QandA_window_QGroupBox)
-        self.QandA.setObjectName("QandA")
-        self.QandA.setFixedSize(780, 550)
-        self.QandA.move(2, 2)
-        text = open('qanda.txt').read()
-        self.QandA.setPlainText(text)
-        self.QandA.setReadOnly(True)
+        # self.QandA_window_QGroupBox = QtWidgets.QGroupBox(self.QandA_window_central_widget)
+        # self.QandA_window_QGroupBox.setFixedSize(784, 554)
+        # self.QandA_window_QGroupBox.move(8, 35)
+        # self.QandA_window_QGroupBox.setEnabled(True)
+        # self.QandA_window_QGroupBox.setFlat(True)
+        # self.QandA_window_QGroupBox.setObjectName("QandATextContainer")
+        #
+        # self.QandA_QLabel = self.create_QLabel("QandA_window_QGroupBox", 343, 0, 200, 50)
+        # self.QandA_QLabel.setText("QandA")
+        # self.QandA_QLabel.setObjectName("QandATitle")
+        #
+        # self.QandA = QtWidgets.QPlainTextEdit(self.QandA_window_QGroupBox)
+        # self.QandA.setObjectName("QandA")
+        # self.QandA.setFixedSize(780, 550)
+        # self.QandA.move(2, 2)
+        # text = open('qanda.txt').read()
+        # self.QandA.setPlainText(text)
+        # self.QandA.setReadOnly(True)
         self.QandA_window.show()
 
     # Show the maps window for around the user's location, based on their latitude and longitude
